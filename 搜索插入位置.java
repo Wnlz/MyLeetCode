5a77ee97732296d73a66b82ca0658eb9fe191ca3
LIZHI <89562684+Wnlz@users.noreply.github.com>
2025-02-14 09:26:56 +0800

    Update 搜索插入位置.java

--- 搜索插入位置.java
+++ 搜索插入位置.java
@@ -1,3 +1,4 @@
+/*
 给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
 要求必须使用时间复杂度为 O(log n) 的算法。
 
@@ -6,8 +7,9 @@
 -10^4 <= nums[i] <= 10^4
 nums 为 无重复元素 的 升序 排列数组
 -10^4 <= target <= 10^4
+*/
 
-答案：
+// 1.答案：
 class Solution {
     public int searchInsert(int[] nums, int target) {
         // 二分法
@@ -27,7 +29,9 @@ class Solution {
     }
 }
 
+/*
 作者：王二三
 链接：https://leetcode.cn/leetbook/read/array-and-string/cxqdh/?discussion=g0UsEI
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
